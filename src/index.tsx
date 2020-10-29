37bf398fb9f89db9d14635bfc07e971ed76a0d99
kaplantm <tkaplan@rightpoint.com>
2020-10-29 16:34:45 -0400

    add material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { PersistGate } from "redux-persist/integration/react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "fontsource-roboto";
 import "./index.css";
 import App from "./App";
 import { store, persistor } from "./lib/redux/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
